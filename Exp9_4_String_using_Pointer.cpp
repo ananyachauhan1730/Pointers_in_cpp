67f9ca903474582c75855c5c534da70848aebb2a
ananyachauhan1730 <chauhanananya603@gmail.com>
2025-08-18 01:27:25 +0530

    Create Exp9_4_String_using_Pointer.cpp

---
+++ Exp9_4_String_using_Pointer.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string str1="";
+
+    cout<<"Enter the String: ";
+    cin>>str1;
+
+    string* str_ptr=&str1;
+
+    cout<<"Entered String is: "<<*str_ptr;
+
+    return 0;
+}
+
+
+/*
+Output:
+Enter the String: Ananya
+Entered String is: ananya
+*/
